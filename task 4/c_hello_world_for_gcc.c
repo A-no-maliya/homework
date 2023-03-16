c7e32f61c9b6af6c480d3092a3f6a05c6b1ec31b
Amaliya Romanova <1132226167@pfur.ru>
2023-03-16 12:21:18 +0300

    add folder and file

---
+++ task 4/c_hello_world_for_gcc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int narg, char* arg[])                  // the first arg is how many arg heva main
+{
+    printf("Hello, %i!\n", narg);
+    
+    getchar();
+    return 0;
+    
+}
